e631441fe766d066d69089341f1279174dacda62
BobaPechedaskez <71347639+BobaPechedaskez@users.noreply.github.com>
2023-02-12 18:14:59 +0300

    For circles 5 task

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,21 @@
-import java.text.DecimalFormat;
-import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float sum = 0;
-        int j =0;
         int N = scanner.nextInt();
-        int[] m = new int[N];
-        for (int i = 0; i < N; i++) {
-            m[i] = scanner.nextInt();
+        int[] a = new int[N];
+        int min;
+        for (int i = 0; i<N; i++){
+            int b = scanner.nextInt();
+            a[i]=b;
+        }
+        for (int i = 0; i < N/2; i++) {
+            min = a[i];
+            a[i]=a[N-i-1];
+            a[N-i-1]=min;
         }
         for (int i = 0; i < N; i++) {
-            if (m[i]%2!=0) {
-                sum+=m[i];
-                j++;
-            }
-        }
-        if (j==0) {
-            System.out.println("NO");
-        } else {
-            System.out.printf(Locale.US,"%.2f", sum/j);
+            System.out.print(a[i]+" ");
         }
     }
 }
